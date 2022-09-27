430f15358cebd765231263c4ce314e19f9874b2f
Daniel Courvoisier <daniel.courvoisier@protonmail.ch>
2022-09-27 09:56:41 +0200

    first commit

---
+++ install_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# To install on start of server
+#sudo apt -y install git
+#cd /home/ubuntu/
+#git clone https://github.com/Danielcourvoisier/cloudsys-lab1-client.git
+#export CLOUDSYS_SERVER_IP=google.ch
+
+# Run Client
+cd /home/ubuntu/cloudsys-lab1-client/
+python3 main.py
